f484f8aab68f9555bcd1c74381a2f46f565dde76
karywjh <jiahuiw4@illinois.edu>
2020-09-09 17:57:40 +0800

    set up global and home scss

--- website-reboot/pages/index.js
+++ website-reboot/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import Layout, { siteTitle } from '../components/layout'
 
 export default function Home() {
