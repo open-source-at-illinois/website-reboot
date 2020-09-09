95f23902e5783521b69783e4cf24e4ed89f97fea
karywjh <jiahuiw4@illinois.edu>
2020-09-09 16:49:37 +0800

    created layout

---
+++ website-reboot/components/layout.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from './layout.module.scss'
+import Link from 'next/link'
+
+export const siteTitle = 'OSAI'
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Official website for Open Source at Illinois Club"
+        />
+      </Head>
+      <header className={styles.navbar}>
+        <Link href="/">
+            <a className={styles.link}>Home</a>
+        </Link>
+        <Link href="/announcements">
+            <a className={styles.link}>Announcements</a>
+        </Link>
+        <Link href="/projects">
+            <a className={styles.link}>Projects</a>
+        </Link>
+        <Link href="/workshops">
+            <a className={styles.link}>Workshops</a>
+        </Link>
+        <Link href="/members">
+            <a className={styles.link}>Members</a>
+        </Link>
+      </header>
+
+      <main className={styles.main}>{children}</main>
+
+      <footer>
+        <p>Email us: blabla@osai-web.com</p>
+      </footer>
+    </div>
+  )
+}
