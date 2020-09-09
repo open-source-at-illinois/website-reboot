b2a55cd2f0eee0f37a9c0d95f02ccf920a11f959
karywjh <jiahuiw4@illinois.edu>
2020-09-09 18:32:16 +0800

    updated home page

--- website-reboot/pages/index.js
+++ website-reboot/pages/index.js
@@ -8,6 +8,27 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-    </Layout>
+
+      <section className={styles.container}>
+        <img src="/images/Logo.png" className={styles.image}/>
+
+        <section className={styles.homeText}>
+          <h1>WELCOME</h1>
+          <h2>ABOUT OPEN SOURCE</h2>
+          <p>
+            We are an RSO registered with UIUC dedicated to developing open-source 
+            software to promote a healthy and free internet. 
+          </p>
+          <h2>ANNOUNCEMENTS</h2>
+          <p>
+            First general meeting this Saturday (2020.9.5)!
+          </p>
+          <h2>DISCORD</h2>
+          <p>
+            Come join our active development community on Discord at <a href='https://discord.gg/yumdCzf' target="_blank">https://discord.gg/yumdCzf</a>! We collaborate on projects, post helpful articles on various topics.
+          </p>
+        </section>
+      </section>
+    </Layout>  
   )
 }
