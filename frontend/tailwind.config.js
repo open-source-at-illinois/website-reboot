5a4b9fa965f61323d6de81469b6516f2d3212512
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-18 17:55:23 -0500

    Add colors and default font to config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "darkBlue": "#16224c",
+          "lightBlue": "#3F60D9",
+          "darkOrange": "#d09a22",
+          "lightOrange": "#FFC12C",
+        }
+      }
+    },
+    fontFamily: {
+      'sans': ['Comfortaa', 'sans-serif'],
+    }
   },
   plugins: [],
 }
