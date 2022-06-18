aa8ae9b5b2ce97b565f0f7cfef0bb56ed0301e8d
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-18 17:56:07 -0500

    Add calendar page

---
+++ frontend/pages/calendar.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+
+
+const Calendar: NextPage = () => {
+    return (
+        <>
+            <header>
+                <title>OSAI Calendar</title>
+            </header>
+            <main>
+                <div className="flex flex-col text-center w-full">
+                    Calendar
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Calendar;
+
