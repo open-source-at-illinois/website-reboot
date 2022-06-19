646184abfd9c57b547140fd2f382430dc7980484
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-19 14:41:14 -0500

    Reduce calendar iframe size

--- frontend/pages/calendar.tsx
+++ frontend/pages/calendar.tsx
@@ -21,8 +21,8 @@ const Calendar: NextPage = () => {
                     </p>
                     <CalendarButton />
                 </div>
-                <div style={{ width: '100vw', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FChicago&title&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&showDate=1&mode=MONTH&src=a3M4ZWt0czBtaHFjdjNnZGhoOTltNWtqZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5" style={iframeStyle} width="800" height="600" scrolling="no"></iframe>
+                <div className="hidden md:flex w-full justify-center flex-col items-center">
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FChicago&title&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&showDate=1&mode=MONTH&src=a3M4ZWt0czBtaHFjdjNnZGhoOTltNWtqZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5" style={iframeStyle} width="600" height="600" scrolling="no"></iframe>
                 </div>
             </main>
         </>
