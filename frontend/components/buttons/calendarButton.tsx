3fcf30dd57fc15d0d6aee42c17514888b943fd85
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-19 14:27:25 -0500

    Add calendar button to calendar page

--- frontend/components/buttons/calendarButton.tsx
+++ frontend/components/buttons/calendarButton.tsx
@@ -2,12 +2,13 @@ import { Icon } from "@iconify/react";
 
 const CalendarButton = () => {
     return (
-        <button className="flex flex-row text-white bg-primary-lightOrange hover:bg-primary-darkOrange duration-300 rounded-lg items-center gap-4 p-2">
+        <a className="flex flex-row text-white bg-primary-lightOrange hover:bg-primary-darkOrange duration-300 rounded-lg items-center gap-4 p-2"
+            href="https://bit.ly/3AOYya8">
             <Icon icon="akar-icons:calendar" color="white" className='text-xl' />
             <div className='mr-2'>
                 Add Calendar
             </div>
-        </button>
+        </a>
     )
 }
 
--- frontend/pages/calendar.tsx
+++ frontend/pages/calendar.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import CalendarButton from "../components/buttons/calendarButton";
 
 
 const Calendar: NextPage = () => {
@@ -11,8 +12,17 @@ const Calendar: NextPage = () => {
                 <title>OSAI Calendar</title>
             </header>
             <main>
+                <div className="flex flex-col my-5 text-center items-center">
+                    <h1 className="text-xl my-2">
+                        Meetings, events, socials. All in one place.
+                    </h1>
+                    <p className="mb-2">
+                        Add the Open-Source at Illinois Calendar and stay updated with the latest events.
+                    </p>
+                    <CalendarButton />
+                </div>
                 <div style={{ width: '100vw', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FChicago&title&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&showDate=1&mode=MONTH&src=a3M4ZWt0czBtaHFjdjNnZGhoOTltNWtqZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5" style={iframeStyle} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FChicago&title&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&showDate=1&mode=MONTH&src=a3M4ZWt0czBtaHFjdjNnZGhoOTltNWtqZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%233F51B5" style={iframeStyle} width="800" height="600" scrolling="no"></iframe>
                 </div>
             </main>
         </>
