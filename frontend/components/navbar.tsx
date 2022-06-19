d5dadb5413e18ea6a72192c56065a1bbda7b3783
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-18 19:37:23 -0500

    Add links to navbar items

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,15 +1,19 @@
-import { Icon } from '@iconify/react';
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className={`flex fixed h-20 text-xl flex-row px-10 w-full top-0 bg-gray-100 text-primary-darkBlue justify-between items-center my-0`}>
-            <h1>
-                Open-Source @ Illinois
-            </h1>
+        <div className={`flex fixed h-20 text-xl flex-row px-10 w-full top-0 text-primary-darkBlue justify-between items-center my-0 z-30`}>
+            <Link href="/">
+                <a>
+                    Open-Source @ Illinois
+                </a>
+            </Link>
             <div className='flex gap-10'>
-                <div>Calendar</div>
-                <div>Github</div>
-                <div>Discord</div>
+                <Link href='/calendar'>
+                    <a>Calendar</a>
+                </Link>
+                <a href='https://github.com/open-source-at-illinois'>Github</a>
+                <a href='https://discord.gg/sKYtc9QhuP '>Discord</a>
             </div>
         </div>
     )
