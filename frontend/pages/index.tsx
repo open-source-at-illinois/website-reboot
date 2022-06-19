0a0772d255414459731189787c29f83abab1b3f9
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-18 19:38:35 -0500

    Add 'Get Involved' section to landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import openSourceSvg from '../public/open-source.svg'
 import { Icon } from '@iconify/react';
+import blob from '../public/blob.svg'
+import DiscordButton from '../components/buttons/discordButton';
+import GithubButton from '../components/buttons/githubButton';
+import CalendarButton from '../components/buttons/calendarButton';
 
 const Home: NextPage = () => {
   return (
@@ -10,12 +14,12 @@ const Home: NextPage = () => {
       <Head>
         <title>Open-Source at Illinois</title>
         <meta name="description" content="All things Open-Source" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-32.png" />
       </Head>
 
-      <main className="flex flex-col h-screen bg-slate-100 font-sans">
+      <main className='flex flex-col h-screen font-sans'>
         {/* Hero section */}
-        <div className='flex flex-col-reverse gap-10 md:flex-row items-center mx-10 p-5'>
+        <section className='flex flex-col-reverse gap-10 md:flex-row items-center mx-10 p-5'>
           <div className='justify-center p-5'>
             <Image className='h-40' src={openSourceSvg} alt='Illustration of Open-source Software' />
           </div>
@@ -26,20 +30,35 @@ const Home: NextPage = () => {
             <p>
               Open-Source software runs our smart watches, elevators, traffic signals and countless other systems we use every day.
             </p>
-            <p>
+            <p className='my-1'>
               At Open-Source @ Illinois, we take a deep dive into the engineering, ethics and philosophy that deeply impact the technological world.
               If that sounds like something you want to be a part of, we're excited to have you.
             </p>
-            <button className="flex my-4 flex-row content-center text-white bg-primary-lightBlue hover:bg-primary-darkBlue duration-300 rounded-lg w-1/4 items-center gap-4 p-2">
-            <Icon icon="akar-icons:discord-fill" color="white" className='text-xl'/>
-              <div>
-                Join the Discord
-              </div>
-            </button>
+            <div className="flex flex-row gap-5 my-4 justify-center md:justify-start w-full">
+              <DiscordButton />
+              <GithubButton />
+            </div>
           </div>
-        </div>
+        </section>
+        {/* Get Involved Section */}
+        <section className="flex justify-center blob items-center text-center h-[50vh]">
+          <div className='flex flex-col'>
+            <h3 className='text-2xl font-bold text-gray-900 my-4 '>
+              How do I get involved?
+            </h3>
+            <p className=''>
+              Open-Source at Illinois holds weekly meetings and special events throughout the year.
+            </p>
+            <p>
+              To get started, just join our discord server. To stay updated, add the open-source calendar.
+            </p>
+            <div className="flex flex-row gap-5 my-4 justify-center align-center w-full">
+              <DiscordButton />
+              <CalendarButton />
+            </div>
+          </div>
+        </section>
 
-        
       </main>
     </>
   )
