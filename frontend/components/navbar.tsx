1dc50c0e3b402624e153a57b53b7daa6e75bcaba
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-19 14:40:52 -0500

    Add temporary mobile navbar

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -2,20 +2,29 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className={`flex fixed h-20 bg-white text-xl flex-row px-10 w-full top-0 text-primary-darkBlue justify-between items-center my-0 z-30`}>
+        <header>
+        <div className={`sm:hidden md:flex fixed h-20 bg-white text-xl flex-row px-10 w-full top-0 text-primary-darkBlue justify-between items-center my-0 z-30`}>
             <Link href="/">
-                <a>
+                <a className='hover:text-primary-lightBlue duration-300'>
                     Open-Source @ Illinois
                 </a>
             </Link>
             <div className='flex gap-10'>
                 <Link href='/calendar'>
-                    <a>Calendar</a>
+                    <a className='hover:text-primary-lightBlue duration-300'>Calendar</a>
                 </Link>
-                <a href='https://github.com/open-source-at-illinois'>Github</a>
-                <a href='https://discord.gg/sKYtc9QhuP'>Discord</a>
+                <a className='hover:text-primary-lightBlue duration-300' href='https://github.com/open-source-at-illinois'>Github</a>
+                <a className='hover:text-primary-lightBlue duration-300' href='https://discord.gg/sKYtc9QhuP'>Discord</a>
             </div>
         </div>
+        <div className={`flex md:hidden fixed h-20 bg-white text-xl flex-row px-10 w-full top-0 text-primary-darkBlue justify-between items-center my-0 z-30`}>
+            <Link href="/">
+                <a className='hover:text-primary-lightBlue duration-300'>
+                    Open-Source @ Illinois
+                </a>
+            </Link>
+        </div>
+        </header>
     )
 }
 
