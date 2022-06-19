1881fc340436228a0933eba008150e6723e62a6d
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-19 12:21:31 -0500

    Use github icon for GithubButton

--- frontend/components/buttons/githubButton.tsx
+++ frontend/components/buttons/githubButton.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 const GithubButton = () => {
     return (
         <button className="flex flex-row text-primary-darkOrange border-2 border-primary-darkOrange hover:border-primary-lightOrange duration-300 rounded-lg items-center gap-4 p-2">
-            <Icon icon="akar-icons:discord-fill" className='text-xl' color="orange" />
+            <Icon icon="akar-icons:github-fill" className='text-xl' color="orange" />
             <div className='mr-2'>
                 Explore our Github
             </div>
