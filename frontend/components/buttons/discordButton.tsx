72c6914a9fdaa0362229aaab006c2a2f8c8ecf25
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-19 12:36:30 -0500

    Add links to github & discord buttons

--- frontend/components/buttons/discordButton.tsx
+++ frontend/components/buttons/discordButton.tsx
@@ -2,12 +2,13 @@ import { Icon } from "@iconify/react";
 
 const DiscordButton = () => {
     return (
-        <button className="flex flex-row text-white bg-primary-lightBlue hover:bg-primary-darkBlue duration-300 rounded-lg items-center gap-4 p-2">
+        <a className="flex flex-row text-white bg-primary-lightBlue hover:bg-primary-darkBlue duration-300 rounded-lg items-center gap-4 p-2"
+            href="https://discord.gg/sKYtc9QhuP">
             <Icon icon="akar-icons:discord-fill" color="white" className='text-xl' />
             <div className='mr-2'>
                 Join the Discord
             </div>
-        </button>
+        </a>
     )
 }
 
