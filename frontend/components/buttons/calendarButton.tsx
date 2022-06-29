c73ce80079361620148bb8e4b73e3ec5b1babed5
Soham Kulkarni <sohamkrox@gmail.com>
2022-06-29 00:20:52 -0500

    Add scale animation to buttons

--- frontend/components/buttons/calendarButton.tsx
+++ frontend/components/buttons/calendarButton.tsx
@@ -1,13 +1,17 @@
 import { Icon } from '@iconify/react';
+import ScaleOnHover from '../animations/scaleOnHover';
+import SubtleScale from '../animations/subtleScale';
 
 const CalendarButton = () => {
   return (
-    <a
-      className='flex flex-row text-white bg-primary-lightOrange hover:bg-primary-darkOrange duration-300 rounded-lg items-center gap-4 p-2'
-      href='https://bit.ly/3AOYya8'>
-      <Icon icon='akar-icons:calendar' color='white' className='text-xl' />
-      <div className='mr-2'>Add Calendar</div>
-    </a>
+    <ScaleOnHover>
+      <a
+        className='flex flex-row text-white bg-primary-lightOrange border-2 border-primary-lightOrange hover:border-primary-darkOrange hover:bg-primary-darkOrange duration-300 rounded-lg items-center gap-4 p-2'
+        href='https://bit.ly/3AOYya8'>
+        <Icon icon='akar-icons:calendar' color='white' className='text-xl' />
+        <div className='mr-2'>Add Calendar</div>
+      </a>
+    </ScaleOnHover>
   );
 };
 
--- frontend/components/buttons/githubButton.tsx
+++ frontend/components/buttons/githubButton.tsx
@@ -1,13 +1,22 @@
 import { Icon } from '@iconify/react';
+import { motion } from 'framer-motion';
+import ScaleOnHover from '../animations/scaleOnHover';
+import SubtleScale from '../animations/subtleScale';
 
 const GithubButton = () => {
   return (
-    <a
-      className='flex flex-row text-primary-darkOrange border-2 border-primary-darkOrange hover:border-primary-lightOrange hover:text-primary-lightOrange duration-300 rounded-lg items-center gap-4 p-2'
-      href='https://github.com/open-source-at-illinois'>
-      <Icon icon='akar-icons:github-fill' className='text-xl' color='orange' />
-      <div className='mr-2'>Explore our Github</div>
-    </a>
+    <ScaleOnHover>
+      <a
+        className='flex flex-row text-primary-darkOrange border-2 border-primary-darkOrange hover:border-primary-lightOrange hover:text-primary-lightOrange duration-300 rounded-lg items-center gap-4 p-2'
+        href='https://github.com/open-source-at-illinois'>
+        <Icon
+          icon='akar-icons:github-fill'
+          className='text-xl'
+          color='orange'
+        />
+        <div className='mr-2'>Explore our Github</div>
+      </a>
+    </ScaleOnHover>
   );
 };
 
