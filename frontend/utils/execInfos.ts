234fffeb08086a431f65d3f284ad3592ddc7a7f4
Soham Kulkarni <sohamkrox@gmail.com>
2022-08-19 20:55:32 -0500

    Separate execInfo into independent file

---
+++ frontend/utils/execInfos.ts
@@ -0,0 +1,88 @@
+import { ExecInfo } from '../pages/about';
+
+const execInfos: ExecInfo[] = [
+  {
+    name: 'Soham Kukarni',
+    email: 'sohamk2@illinois.edu',
+    designation: 'President',
+    bio: "Hi! I’m Soham, and I’m a rising Junior. I’m super passionate  \
+      about new technologies and using them for social good. That’s       \
+      one of the reasons why I love open-source software. I'm really      \
+      good at making dad jokes and building things with code, so if       \
+      either of those interst you, I'm more than happy to hang out and    \
+      chat :)",
+    headshot: '/../public/headshots/soham.jpg',
+  },
+  {
+    name: 'Max Fan',
+    email: 'myfan3@illinois.edu',
+    designation: 'Vice President',
+    bio: '',
+    headshot: '',
+  },
+  {
+    name: 'Arnav Shah',
+    email: 'arnavss2@illinois.edu',
+    designation: 'Treasurer',
+    bio: '',
+    headshot: '',
+  },
+  {
+    name: 'Athena Fung',
+    email: 'affung2@illinois.edu',
+    designation: 'Secretary',
+    bio: '',
+    headshot: '',
+  },
+  {
+    name: 'Kiran Kaur',
+    email: 'kaur18@illinois.edu',
+    designation: 'Membership Director',
+    bio: "Hi, I'm Kiran! I'm a Junior in Industrial Engineering (but some may \
+        call me a fake CS major). I love learning new skills. Recently, I picked\
+         up ukulele and have really enjoyed that journey! I am also a very extroverted\
+          person and I talk A LOT as you will find out at Open Source events!",
+    headshot: '',
+  },
+  {
+    name: 'Anagha Tiwari',
+    email: 'anaghat3@illinois.edu',
+    designation: 'Freshman Representative',
+    bio: 'Hello :)) My name is Anagha, and I’m a freshman majoring in CS & \
+      Statistics. This year, I am super excited to meet fellow Illini from diverse\
+       majors, cultures, and experiences, while also developing my academic interests \
+       at the university. Specifically at Open Source at Illinois, I hope to be a part \
+       of a welcoming and supportive environment where we can collaborate with one another \
+       to create projects and contribute to the open source community! During my free time, \
+       I love taking walks around campus, playing cards with friends, swimming, working out, \
+       and eating spicy food!',
+    headshot: '',
+  },
+  {
+    name: 'Prakhar Gupta',
+    email: 'prakhar7@illinois.edu',
+    designation: 'Freshman Representative',
+    bio: " I'm Prakhar, an incoming freshman. I'm going to be majoring \
+      in Electrical Engineering and will be staying at ISR this year.    \
+      I play the guitar and enjoy listening to hip-hop music. \
+      I'm also into cooking and a bit of basketball. I'm \
+      excited to be a part of Open Source at Illinois this fall, \
+      and I can't wait to get started!",
+    headshot: '/../public/headshots/prakhar.jpg',
+  },
+  {
+    name: 'Akshata Tiwari',
+    email: 'atiwari5@illinois.edu',
+    designation: 'Freshman Representative',
+    bio: 'Hi everyone! I am a freshman studying Computer Science +\
+       Economics this fall. I love to paint, play poker, work out, and\
+        eat chipotle bowls. I am passionate about social outreach initiatives\
+         that enable digital advocacy and transparency, something I hope to\
+          continue at Open Source! I am excited to explore campus opportunities,\
+           programs, and cool RSOs while meeting new friends along the way, and I\
+            can’t wait to become part of the open source community with all of you!',
+    headshot: '',
+  },
+];
+
+export default execInfos;
