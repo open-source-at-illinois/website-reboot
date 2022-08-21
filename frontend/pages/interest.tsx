7d9595bb5ba5e5dcdfe89c856116c83fc6b5e7fc
Soham Kulkarni <sohamkrox@gmail.com>
2022-08-21 10:48:38 -0500

    Improve mobile view for interest form

--- frontend/pages/interest.tsx
+++ frontend/pages/interest.tsx
@@ -2,12 +2,12 @@ import Loading from '../components/views/loading';
 
 const Interest = () => {
   return (
-    <div className='h-full'>
+    <div className='h-full overflow-x-auto'>
       <iframe
         className='mx-auto'
         src='https://docs.google.com/forms/d/e/1FAIpQLSfSNE6Fiz-lcRnkjX0MZFQE_69qzP9Vq6HrdfOzIUDVx9CD2Q/viewform?embedded=true'
-        width='640'
-        height='762'>
+        width='500'
+        height='770'>
         <Loading />
       </iframe>
     </div>
