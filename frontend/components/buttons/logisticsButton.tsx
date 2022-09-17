0a56d63886672f04820cf3cd43f8cacd28a61e7b
Soham Kulkarni <sohamkrox@gmail.com>
2022-09-17 18:20:50 -0500

    Point logisticsButton to new README page

--- frontend/components/buttons/logisticsButton.tsx
+++ frontend/components/buttons/logisticsButton.tsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const LogisticsButton = () => {
   return (
-    <Link href='/logistics'>
+    <Link href='/README'>
       <a className='bg-primary-lightBlue text-white hover:bg-primary-darkBlue duration-300 rounded-md p-2 m-1'>
         <div className='flex flex-row items-center'>
           Learn how we do things
